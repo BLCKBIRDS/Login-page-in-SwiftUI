54141cce4022258dc01735fa7ec8225666b2402d
BLCKBIRDS <andreasschultz@Andreass-MacBook-Pro.local>
2019-07-06 18:16:29 -0700

    Added the whole project

---
+++ LoginPageSwiftUITutorial/ContentView.swift
@@ -0,0 +1,137 @@
+//
+//  ContentView.swift
+//  LoginPageSwiftUITutorial
+//
+//  Created by BLCKBIRDS on 01.07.19.
+//  Copyright © 2019 BLCKBIRDS. All rights reserved.
+//
+
+import SwiftUI
+
+let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
+
+let storedUsername = "Myusername"
+let storedPassword = "Mypassword"
+
+struct ContentView : View {
+    
+    @State var username: String = ""
+    @State var password: String = ""
+    
+    @State var authenticationDidFail: Bool = false
+    @State var authenticationDidSucceed: Bool = false
+    
+    @State var editingMode: Bool = false
+    
+    var body: some View {
+        
+        ZStack {
+            VStack {
+                WelcomeText()
+                UserImage()
+                UsernameTextField(username: $username, editingMode: $editingMode)
+                PasswordSecureField(password: $password)
+                if authenticationDidFail {
+                    Text("Information not correct. Try again.")
+                        .offset(y: -10)
+                        .foregroundColor(.red)
+                }
+                Button(action: {
+                    if self.username == storedUsername && self.password == storedPassword {
+                        self.authenticationDidSucceed = true
+                        self.authenticationDidFail = false
+                    } else {
+                        self.authenticationDidFail = true
+                    }
+                }) {
+                    LoginButtonContent()
+                }
+                }
+                .padding()
+            if authenticationDidSucceed {
+                Text("Login succeeded!")
+                    .font(.headline)
+                    .frame(width: 250, height: 80)
+                    .background(Color.green)
+                    .cornerRadius(20.0)
+                    .foregroundColor(.white)
+                    .animation(.basic(duration: 0.5, curve: .easeIn))
+            }
+        }
+            .offset(y: editingMode ? -150 : 0)
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
+
+struct WelcomeText : View {
+    var body: some View {
+        return Text("Welcome!")
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+            .padding(.bottom, 20)
+    }
+}
+
+struct UserImage : View {
+    var body: some View {
+        return Image("userImage")
+            .resizable()
+            .aspectRatio(UIImage(named: "userImage")!.size, contentMode: .fill)
+            .frame(width: 150, height: 150)
+            .clipped()
+            .cornerRadius(150)
+            .padding(.bottom, 75)
+    }
+}
+
+struct LoginButtonContent : View {
+    var body: some View {
+        return Text("LOGIN")
+            .font(.headline)
+            .color(.white)
+            .padding()
+            .frame(width: 220, height: 60)
+            .background(Color.green)
+            .cornerRadius(15.0)
+    }
+}
+
+struct UsernameTextField : View {
+    
+    @Binding var username: String
+    
+    @Binding var editingMode: Bool
+    
+    var body: some View {
+        return TextField($username, placeholder: Text("Username"), onEditingChanged: {edit in
+            if edit == true
+            {self.editingMode = true}
+            else
+            {self.editingMode = false}
+        })
+            .padding()
+            .background(lightGreyColor)
+            .cornerRadius(5.0)
+            .padding(.bottom, 20)
+    }
+}
+
+struct PasswordSecureField : View {
+    
+    @Binding var password: String
+    
+    var body: some View {
+        return SecureField($password, placeholder: Text("Password"))
+            .padding()
+            .background(lightGreyColor)
+            .cornerRadius(5.0)
+            .padding(.bottom, 20)
+    }
+}
